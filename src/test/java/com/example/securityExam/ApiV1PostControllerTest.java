ba862c116d2fbc4e7985b821e94a677bc535c26a
chacha86 <taejincha0619@gmail.com>
2025-02-07 10:19:32 +0900

    setLogin

--- src/test/java/com/example/securityExam/ApiV1PostControllerTest.java
+++ src/test/java/com/example/securityExam/ApiV1PostControllerTest.java
@@ -308,7 +308,7 @@ public class ApiV1PostControllerTest {
     @WithUserDetails("user2")
     void write1() throws Exception {
 
-        String apiKey = "user1";
+        String apiKey = "user3";
         String title = "새로운 글 제목";
         String content = "새로운 글 내용";
 
